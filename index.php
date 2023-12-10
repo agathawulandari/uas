3a108747d8c3f4b2fca272d05697e1fcdb5aceda
iamalya <alyasama226@gmail.com>
2023-12-10 17:28:14 +0700

    halaman harga

--- index.php
+++ index.php
@@ -105,58 +105,10 @@
 
 <body>
 
-
-    <header data-bs-theme="dark">
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Carousel</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=harga">Harga</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-    <main role="main">
-
-    </main>
-    <div class="mt-5 py-5">
-        <div class="container">
-            <?php 
-            if (isset($_GET['page'])) {
-                switch ($_GET['page']) {
-                    case 'harga':
-                        include('harga.php');
-                        break;
-                    
-                    default:
-                        'Home';
-                        include('index.php');
-                        break;
-                }
-            }
+    <?php 
+include('navbar.php');
 ?>
-        </div>
 
-    </div>
     <main>
         <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
             <div class="carousel-indicators">
