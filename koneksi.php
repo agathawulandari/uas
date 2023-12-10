defd1e20ee5cf9695e85f380106eb43ec062b6f7
hidayatullas <sity111120022@gmail.com>
2023-12-10 15:59:37 +0700

    tambah assets

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","gaji");
+$koneksi = mysqli_connect("localhost","root","","coba_uas");
 
 if (mysqli_connect_errno()) {
     echo "Koneksi error". mysqli_connect_error();
