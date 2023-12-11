f5dde6b52257168a4d2dabc3256885d42d1f7742
iamalya <alyasama226@gmail.com>
2023-12-11 23:51:49 +0700

    membuat halaman pilihan lapangan

---
+++ pengguna/lapangan.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <header>
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal text-body-emphasis">Lapangan</h1>
+            <p class="fs-5 text-body-secondary">Pilih No Lapangan Yang Anda Inginkan</p>
+        </div>
+    </header>
+    <div class="card text-center mb-3" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+<?php 
+ include('index.php');
+ ?>
