c23e6a570cc506a4d8a1c51a233d496611261ca8
hidayatullas <sity111120022@gmail.com>
2023-12-11 21:56:56 +0700

    update contact

---
+++ umum/contact.php
@@ -0,0 +1,67 @@
+<div class="container">
+    <h2 class="text-center py-5">Contact</h2>
+
+    <div class="row text-center">
+        <div class="col">
+            <div class="card bg-white text-dark">
+                <div class="card-header">
+                    <h3> Lokasi </h3>
+                </div>
+                <div class="card-body">
+                    <p>Jl. Merdeka No. 123, Cipta Karya Kota Bahagia Indonesia 12345</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-link text-dark">Detail</a>
+                </div>
+            </div> 
+        </div>
+        <div class="col">
+            <div class="card bg-white text-dark">
+                <div class="card-header">
+                    <h3> Hubungi </h3>
+                </div>
+                <div class="card-body">
+                    <p>
+                        082169913673
+                        (Admin)
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-link text-dark">Detail</a>
+                </div>
+            </div> 
+        </div>
+        <div class="col">
+            <div class="card bg-white text-dark">
+                <div class="card-header">
+                    <h3> Buka </h3>
+                </div>
+                <div class="card-body">
+                    <p>08.00 - 22.00</p>
+                    <p>Setiap Hari</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-link text-dark">Detail</a>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <div class="row text-center mt-3">
+        <div class="col">
+                <div class="card bg-white text-dark">
+                    <div class="card-header">
+                        <h3> Buka </h3>
+                    </div>
+                    <div class="card-body">
+                        <img src="../img/ig.png" alt="ig" width="50">
+                        <img src="../img/fb.png" alt="ig" width="30">
+                        <img src="../img/x.png" alt="ig" width="30">
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-link text-dark">Detail</a>
+                    </div>
+                </div> 
+            </div>
+    </div>
+
+</div>
