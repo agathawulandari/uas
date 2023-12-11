79fc2ab46ba45d7b11a9224c7f7f164f93af89ee
iamalya <alyasama226@gmail.com>
2023-12-11 23:24:13 +0700

    koneksi database kategori

--- pengguna/tipe.php
+++ pengguna/tipe.php
@@ -1,3 +1,7 @@
+<?php 
+include('../koneksi.php');
+?>
+
 <div class="container">
     <header>
         <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
@@ -6,62 +10,35 @@
         </div>
     </header>
     <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-        <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">Reguler</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/jam</small>
-                    </h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>Ukuran lapangan 13,4 m x 6,1 m</li>
-                        <li>untuk tunggal dan ganda</li>
-                        <li>terdapat garis-garis penanda lapangan</li>
-                        <li>tidak memakai alas lapangan</li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">Pesan sekarang</button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">Semi premium</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$15<small
-                            class="text-body-secondary fw-light">/jam</small>
-                    </h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>Ukuran lapangan 13,4 m x 6,1 m</li>
-                        <li>untuk tunggal dan ganda</li>
-                        <li>terdapat garis-garis penanda lapangan</li>
-                        <li>memakai karpet sebagai alas lapangan</li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-primary">Pesan sekarang</button>
-                </div>
-            </div>
-        </div>
+        <?php 
+    $data = mysqli_query($koneksi, "SELECT * FROM kategori");
+    
+    while($d = mysqli_fetch_array($data)) {
+        
+        ?>
         <div class="col">
             <div class="card mb-4 rounded-3 shadow-sm border-primary">
                 <div class="card-header py-3 text-bg-primary border-primary">
-                    <h4 class="my-0 fw-normal">Premium</h4>
+                    <h4 class="my-0 fw-normal"><?php echo $d ['nama_kategori']; ?></h4>
                 </div>
                 <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$29<small
+                    <h1 class="card-title pricing-card-title"><?php echo $d ['harga_kategori']; ?><small
                             class="text-body-secondary fw-light">/jam</small>
                     </h1>
                     <ul class="list-unstyled mt-3 mb-4">
-                        <li>Ukuran lapangan 13,4 m x 6,1 m</li>
-                        <li>memakai karpet sebagai alas lapangan</li>
+                        <li><?php echo $d ['deskripsi']; ?></li>
+                        <!-- <li>memakai karpet sebagai alas lapangan</li>
                         <li>memakai pencahayaan yang lebih baik</li>
-                        <li>kursi-kursi khusus yang nyamanan</li>
+                        <li>kursi-kursi khusus yang nyamanan</li> -->
                     </ul>
                     <button type="button" class="w-100 btn btn-lg btn-primary">Pesan Sekarang</button>
                 </div>
             </div>
         </div>
+        <?php 
+        }
+            
+            ?>
     </div>
 
     <h2 class="display-6 text-center mb-5 mt-5">Fasilitas</h2>
