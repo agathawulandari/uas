84db6ab932d61116e020bf2b9e200440a841ef31
iamalya <alyasama226@gmail.com>
2023-12-12 13:39:33 +0700

    jadwal pemesanan

---
+++ pengguna/jadwal.php
@@ -0,0 +1,22 @@
+<div class="container"></div>
+<header>
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal text-body-emphasis">Jadwal Lapangan</h1>
+        <p class="fs-5 text-body-secondary">Pilih No Lapangan Yang Anda Inginkan</p>
+    </div>
+</header>
+<table class="table table-dark table-striped">
+    <tr>
+        <td>No</td>
+        <td>Nama Pemesan</td>
+        <td>Nomor Lapangan</td>
+        <td>ID Pengguna</td>
+        <td>Tanggal Booking</td>
+        <td>Jam Booking</td>
+        <td>Durasi</td>
+    </tr>
+</table>
+</div>
+<?php 
+ include('index.php');
+ ?>
