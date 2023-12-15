aeb91cfacc9f9b95ce1b8c7489ecf73691b4a847
iamalya <alyasama226@gmail.com>
2023-12-15 21:14:27 +0700

    membuat id

--- pengguna/proses_pesan.php
+++ pengguna/proses_pesan.php
@@ -2,6 +2,11 @@
 include('../koneksi.php');
 
 if (isset($_POST["pesan_lapangan"])) {
+      // untuk pemesanan
+    $query_lapangan = mysqli_query($koneksi, "SELECT MAX(SUBSTRING(id_pemesanan, 2)) AS max_id FROM pemesanan_lapangan");
+    $pemesanan = mysqli_fetch_assoc($query_lapangan);
+    $pemesanan_lapangan = $pemesanan['max_id'] + 1;
+    $id_pemesanan = 'PL' . str_pad($pemesanan_lapangan, 4, '0', STR_PAD_LEFT);
     
     $id_kategori =isset($_POST['id_kategori']) ? $_POST['id_kategori'] : '';
     $id_lapangan = isset($_POST['id_lapangan']) ? $_POST['id_lapangan'] : '';
@@ -10,7 +15,7 @@ if (isset($_POST["pesan_lapangan"])) {
     $jam = isset($_POST['jam']) ? $_POST['jam'] : '';
     $durasi = isset($_POST['durasi']) ? $_POST['durasi'] : '';
     
-   $query = "INSERT INTO pemesanan_lapangan (id_kategori, id_lapangan, id_pengguna, tgl_booking, jam_booking, durasi) VALUES ('$id_kategori', '$id_lapangan','$id_pengguna', '$tanggal', '$jam', '$durasi')";
+   $query = "INSERT INTO pemesanan_lapangan (id_pemesanan,id_kategori, id_lapangan, id_pengguna, tgl_booking, jam_booking, durasi) VALUES ('$id_pemesanan','$id_kategori', '$id_lapangan','$id_pengguna', '$tanggal', '$jam', '$durasi')";
 
        if ($query==true) {
         echo "sukses, silahkan klik disini <a class='btn btn-primary' href='index.php?page=pembayaran'>Disini</a>";
@@ -23,6 +28,12 @@ if (isset($_POST["pesan_lapangan"])) {
 }
 
 if (isset($_POST['bukti'])) {
+     // untuk bayar
+    $query_bayar = mysqli_query($koneksi, "SELECT MAX(SUBSTRING(id_pembayaran, 2)) AS max_id FROM pembayaran");
+    $bayar = mysqli_fetch_assoc($query_bayar);
+    $pembayaran_lapangan = $bayar['max_id'] + 1;
+    $id_pembayaran = 'P' . str_pad($pembayaran_lapangan, 4, '0', STR_PAD_LEFT);
+    
     $id_pemesanan = $_POST['id_pemesanan'];
     $id_pengguna = $_POST['id_pengguna'];
     $id_raket = $_POST['id_raket'];
@@ -35,7 +46,7 @@ if (isset($_POST['bukti'])) {
 
     // move_uploaded_file($id_pengguna, $bukti_bayar_path);
     
-    $query = "INSERT INTO pembayaran (id_pengguna, id_pemesanan, id_raket, metode_pembayaran, status_pembayaran, bukti_bayar, created_at) VALUES ('$id_pengguna', '$id_pemesanan', '$id_raket', '$metode_pembayaran', '$status_pembayaran','$bukti_bayar_path', NOW())";
+    $query = "INSERT INTO pembayaran (id_pembayaran,id_pengguna, id_pemesanan, id_raket, metode_pembayaran, status_pembayaran, bukti_bayar, created_at) VALUES ('$id_pembayaran','$id_pengguna', '$id_pemesanan', '$id_raket', '$metode_pembayaran', '$status_pembayaran','$bukti_bayar_path', NOW())";
 
     mysqli_query($koneksi, $query);
 
