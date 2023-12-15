87938c14b20d264814d1997c0b2c78d627687577
iamalya <alyasama226@gmail.com>
2023-12-15 21:25:09 +0700

    masukin gambar

--- umum/home.php
+++ umum/home.php
@@ -1,16 +1,17 @@
 <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+            aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+            <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            </svg> -->
+            <img src="../gambar/raket-2.jpeg" class="img-fluid" alt="..." width="100%" height="100%">
             <div class="container">
                 <div class="carousel-caption text-start">
                     <h1>Example headline.</h1>
@@ -21,10 +22,11 @@
             </div>
         </div>
         <div class="carousel-item">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+            <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            </svg> -->
+            <img src="../gambar/raket-3.jpeg" class="img-fluid" alt="..." width="100%" height="100%">
             <div class="container">
                 <div class="carousel-caption">
                     <h1>Another example headline.</h1>
@@ -34,10 +36,11 @@
             </div>
         </div>
         <div class="carousel-item">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+            <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            </svg> -->
+            <img src="../gambar/raket-1.jpeg" class="img-fluid" alt="..." width="100%" height="100%">
             <div class="container">
                 <div class="carousel-caption text-end">
                     <h1>One more for good measure.</h1>
@@ -67,36 +70,24 @@
     <!-- Three columns of text below the carousel -->
     <div class="row">
         <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            <img src="../gambar/lapangan-4.jpeg" alt="" class="bd-placeholder-img rounded-circle" width="140"
+                height="140">
             <h2 class="fw-normal">Heading</h2>
             <p>Some representative placeholder content for the three columns of text below the carousel. This is
                 the first column.</p>
             <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            <img src="../gambar/lapangan-5.jpeg" alt="" class="bd-placeholder-img rounded-circle" width="140"
+                height="140">
             <h2 class="fw-normal">Heading</h2>
             <p>Another exciting bit of representative placeholder content. This time, we've moved on to the
                 second column.</p>
             <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-            </svg>
+            <img src="../gambar/lapangan-6.jpeg" alt="" class="bd-placeholder-img rounded-circle" width="140"
+                height="140">
             <h2 class="fw-normal">Heading</h2>
             <p>And lastly this, the third column of representative placeholder content.</p>
             <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
@@ -116,13 +107,16 @@
                 prose here.</p>
         </div>
         <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+            <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
                 height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
                 preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%"
                     fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-            </svg>
+            </svg> -->
+            <img src="../gambar/lapangan-2.jpeg" alt=""
+                class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                height="500">
         </div>
     </div>
 
@@ -130,19 +124,15 @@
 
     <div class="row featurette">
         <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span
-                    class="text-body-secondary">See for yourself.</span></h2>
+            <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See
+                    for yourself.</span></h2>
             <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of
                 how this layout would work with some actual real-world content in place.</p>
         </div>
         <div class="col-md-5 order-md-1">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%"
-                    fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-            </svg>
+            <img src="../gambar/lapangan-3.jpeg" alt=""
+                class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                height="500">
         </div>
     </div>
 
@@ -157,13 +147,9 @@
                 look like with some actual content. Your content.</p>
         </div>
         <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%"
-                    fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-            </svg>
+            <img src="../gambar/lapangan-5.jpeg" alt=""
+                class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                height="500">
         </div>
     </div>
 
