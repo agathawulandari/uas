006ccea5888d142d12d74c31b0ed60d33cb970ae
iamalya <alyasama226@gmail.com>
2023-12-16 22:35:23 +0700

    koneksi ubah

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","uas");
+$koneksi = mysqli_connect("localhost","root","","coba_uas");
 
 if (mysqli_connect_errno()) {
     echo "Koneksi error". mysqli_connect_error();
